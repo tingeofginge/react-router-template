f7655d0802f4cef96c0b3c53a8bda0b40880263a
Brandon Funkhouser <bcfunkhouser@gmail.com>
2025-07-12 13:59:10 -0400

    Fix component reference

--- app/routes/nestedExample.tsx
+++ app/routes/nestedExample.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { AnotherWelcome } from "../anotherWelcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +9,5 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <AnotherWelcome />;
 }
