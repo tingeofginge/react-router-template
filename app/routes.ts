7b52399cc1644d78d582a8888c30c78d33511990
Brandon Funkhouser <bcfunkhouser@gmail.com>
2025-07-12 13:55:56 -0400

    Fix path

--- app/routes.ts
+++ app/routes.ts
@@ -2,5 +2,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"),
-    route("/one/two", "routes/nestedExample.tsx"),
+    route("one/two", "routes/nestedExample.tsx"),
 ] satisfies RouteConfig;
